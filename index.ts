b3c10adf59c7f24e3f23f8b3989a085a5342a2c2
Tom Herman <tomherman11@gmail.com>
2023-07-01 14:39:56 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+function do_something1() {
+    const a = 4;
+    const b = 6;
+    const c = a + b;
+    console.log(c);
+}
+
+function do_something2() {
+    const f = "hello";
+    const g = "world";
+    const h = f + " " + g;
+    console.log(h);
+}
+
+function do_something3() {
+    const q = 4;
+    const r = "world";
+    const s = q + " " + r;
+    console.log(s);
+}
+
